e63ea8adefa98fc625c225225d1d049a7975ab0a
eniopereira-cpu <enio.pereira@gmail.com>
2026-09-22 11:10:32 -0300

    Create sw.js

---
+++ se.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'territorios-v1';
+
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
